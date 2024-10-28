394a902449d1c68458cdfb6974e693768a197b1f
Jakub Markiewicz <markiewicz267@gmail.com>
2024-10-28 17:08:04 +0100

    Add parameters to couriers id in sql table

--- migrations/00000-createTableCouriers.ts
+++ migrations/00000-createTableCouriers.ts
@@ -3,7 +3,7 @@ import type { Sql } from 'postgres';
 export async function up(sql: Sql) {
   await sql`
     CREATE TABLE couriers (
-      id serial PRIMARY KEY,
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
       number int UNIQUE NOT NULL,
       name varchar(30) NOT NULL,
       points int DEFAULT 0,
